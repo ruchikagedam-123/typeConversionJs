f6ebe9799bb3e0a2ca4a20b6721d102f991b5b68
ruchikagedam-123 <67160652+ruchikagedam-123@users.noreply.github.com>
2021-01-09 18:54:02 +0530

    Arithmetic Operators updated with task.

--- ArithmeticOperators.js
+++ ArithmeticOperators.js
@@ -1,3 +1,14 @@
+
+/*
+Task
+
+Complete the following functions in the editor below:
+
+getArea(length, width): Calculate and return the area of a rectangle having sides  and .
+getPerimeter(length, width): Calculate and return the perimeter of a rectangle having sides  and .
+The values returned by these functions are printed to stdout by locked stub code in the editor.
+*/
+
 'use strict';
 
 process.stdin.resume();
